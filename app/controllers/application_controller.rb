37adb5b5fbc352aad6f09a406b1134077c3629ce
Oscar <shoudto@gmail.com>
2019-10-08 16:39:09 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do 
+    erb :layout
+  end 
+
+  get '/signup' do 
+    erb :"/users/signup"
+  end 
+
+  get '/login' do 
+  end 
 end
